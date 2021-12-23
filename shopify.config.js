b99ffe692c1d9c749a30254b831f9d1cc6a27fd8
paula <p.andrescaldevilla@gmail.com>
2021-12-23 16:56:36 +0100

    first commit

---
+++ shopify.config.js
@@ -0,0 +1,6 @@
+export default {
+  locale: 'en-us',
+  storeDomain: 'probandoherramientas.myshopify.com',
+  storefrontToken: '0ba4045d9ae4c34760186845e7347b31',
+  graphqlApiVersion: 'unstable',
+};
